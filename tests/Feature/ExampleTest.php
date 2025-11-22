a47a8a0f4452e1b441b10797718a5a0dfb767627
tmthanhCT <mthanh29122004@gmail.com>
2025-11-22 17:27:57 +0700

    Describe your change

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,8 @@
 <?php
 
 test('the application returns a successful response', function () {
+    /** @var \Tests\TestCase $this */
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertStatus(302);
 });
